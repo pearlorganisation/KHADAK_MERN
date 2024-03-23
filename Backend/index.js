10c933d22e190041e49d6229530374e9e8b84e6d
Gaurav Rajput <gauravrajput@pearlorganisation.com>
2024-03-23 11:23:46 +0530

    created update and get herosection api

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { mongoConnect } from "./src/configs/mongoDB.js";
 import { authRouter } from "./src/routes/Auth/authRoutes.js";
+import heroSectionRouter from "./src/routes/HeroSection/heroSectionroutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 6500;
@@ -11,6 +12,9 @@ mongoConnect();
 app.use(express.json());
 // -----root route for the express app
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/heroSection", heroSectionRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(` app is running on https://localhost:${PORT}`);
---
+++ Backend/src/controllers/HeroSection/heroSectionController.js
@@ -0,0 +1,52 @@
+import heroSectionModel from "../../models/HeroSection/heroSectionModel.js";
+
+// saving herosection data in database
+export const createHeroSection = async (req, res) => {
+  const { title, description } = req.body;
+  const heroSection = new heroSectionModel(req.body);
+  await heroSection
+    .save()
+    .then((hero) => {
+      console.log("hero section created successfully");
+      res.status(200).send(hero);
+    })
+    .catch((error) => console.log(error));
+
+  return res.status(200);
+};
+
+// getting the data from the database
+export const getHeroSection = async (req, res) => {
+  try {
+    const data = await heroSectionModel.find();
+    res.status(200).json({
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
+
+// updating the data of database
+
+export const updateHeroSection = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { title, description, _id } = req.body;
+    const updatedHeroSection = await heroSectionModel.findByIdAndUpdate(
+      _id,
+      { title: title, description: description },
+      { new: true }
+    );
+    res.status(200).json({
+      message: "data updated successfully",
+      data: updatedHeroSection,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
