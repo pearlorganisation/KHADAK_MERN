87fa3f41428a0f981d2be68beda3298ee691952f
Gaurav Rajput <gauravrajput@pearlorganisation.com>
2024-03-26 16:34:20 +0530

    minor changes

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   const [heroSectionData, setHeroSectionData] = useState(null);
   const [footerSectionData, setFooterSectionData] = useState(null);
 
-  const BASE_URL = "https://khadak-mern.onrender.com";
+  const BASE_URL = "https://khadak-mern.onrender.com/api/v1";
   // http://localhost:6500/api/v1
 
   // getting the hero section data
