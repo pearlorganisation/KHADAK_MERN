f7e61db6678ced830b1226d6dde35f5a6715fe77
Gaurav Rajput <gauravrajput@pearlorganisation.com>
2024-04-09 11:54:33 +0530

    fixed the issue, delhi data is now showing when we open the website

--- Frontend/src/components/layout/Layout.jsx
+++ Frontend/src/components/layout/Layout.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./footer/Footer";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { clearState } from "../../features/store";
+import { changeCity, changeLocality } from "../../features/slices/contactSlice";
 
 const Layout = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  if (window.location.pathname === "/") {
+    dispatch(changeCity("Delhi"));
+    dispatch(changeLocality(""));
+  }
+
   return (
     <div className="">
       <Header />
