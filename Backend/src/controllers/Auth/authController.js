c39dd21ebe5e2310ea33c9e71e3093d0d1c12753
Avnish Negi <avnish@pearlorganisation.com>
2024-08-05 16:45:20 +0530

    dvms

--- Backend/src/controllers/Auth/authController.js
+++ Backend/src/controllers/Auth/authController.js
@@ -56,17 +56,48 @@ export const userLogin = async (req, res) => {
         return res.status(400).json({status:false,message:"Incorrect password"})
       }
 
+      const currentDate = new Date();
+
+      await otp.deleteMany({ expiresAt: { $lt: currentDate } },{
+        type:"LOGIN"
+      });
+
       const OTP = generateOtp();
 
       await sendMail(req, res, email, OTP);
 
-      await otp.create({
-        email,
-        otp: OTP,
-        expiresAt: new Date(Date.now() + 300000),
-      });
+      const otpDoc = await otp.findOneAndUpdate(
+        { email, type:"LOGIN" },
+        { otp:OTP, expiresAt: new Date(Date.now() + 300000) },
+        { $new: true }
+      );
 
-      res.json({ success: true, message: "Otp sent successfully!!" });
+   
+      if (!otpDoc) {
+        let doc = new otp({
+          email,
+          type:"LOGIN",
+          otp:OTP,
+          expiresAt: new Date(Date.now() + 300000), //expiry time of otp 5mins
+        });
+
+      await doc.save().then(() => {
+            return res
+              .status(200)
+              .json({ success: true, message: "OTP sent successfully" });
+          });
+        } else {
+            return res
+              .status(200)
+              .json({ success: true, message: "OTP sent successfully" });
+          }
+      // await otp.create({
+      //   email,
+      //   otp: OTP,
+      //   expiresAt: new Date(Date.now() + 300000),
+      // });
+
+      // res.json({ success: true, message: "Otp sent successfully!!" });
     } else {
       // Password is invalid
       res.status(401).json({ success: false, message: "Invalid email" });
@@ -78,4 +109,47 @@ export const userLogin = async (req, res) => {
     });
   }
 };
+
+export const resetPassword = async(req,res)=>{
+  try {
+    
+      const { email, password, confirmPassword } = req.body;
+
+      const user = await userModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email does not exists" });
+    }
+
+
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Password does not match" });
+    }
+    
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await userModel.findOneAndUpdate(
+      { email },
+      { password: hashedPassword },
+      { $new: true }
+    );
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Password Updated Successfully" });
+
+
+  } catch (error) {
+    
+    return res.status(500).json({
+      success: false,
+      message: `Internal Server Error! ${error.message}`,
+    });
+  }
+
+  }
 
--- Backend/src/routes/Auth/authRoutes.js
+++ Backend/src/routes/Auth/authRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createUser, userLogin } from "../../controllers/Auth/authController.js";
+import { createUser, resetPassword, userLogin } from "../../controllers/Auth/authController.js";
 
 export const authRouter = express.Router();
 
 authRouter.route("/addUser").post(createUser);
+authRouter.route("/resetPassword").put(resetPassword);
 authRouter.route("/userLogin").post(userLogin);
